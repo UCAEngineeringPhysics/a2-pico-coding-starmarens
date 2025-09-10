ecc212eb7c8ddc9e1fb55642e3cf399733875829
starmarens <phillips85502@gmail.com>
2025-09-10 14:58:49 -0500

    finished assignment

--- switch_mode.py
+++ switch_mode.py
@@ -1,6 +1,43 @@
-from machine import Pin, PWM, Timer
-from time import sleep
+from machine import Pin,PWM
+from utime import sleep
+#funtions
+mode = 0
+def toggle_mode(hi):
+    #button press changes mode value
+    global mode
+    mode = mode + 1
+    print(mode)
+    if mode >= 3:
+        mode = 0
+    
+                
+    
 
-# SETUP
+def light_on(light):
+    pwm = PWM(light, duty_u16= 65535)
+   
 
-# LOOP
+def light_fade(light):
+    light.value(0)
+    for i in range(0, 65535):
+        pwm = PWM(light, freq = 1000, duty_u16=i)
+        sleep(1/65535)
+            
+
+    for i in range(65535, 0, -1):
+        pwm = PWM(light, freq= 1000, duty_u16=i)
+        sleep(1/65535)
+
+
+
+
+#setup
+light = Pin(15, Pin.OUT)
+detection = Pin(14, Pin.IN, Pin.PULL_UP)
+detection.irq(trigger= light.IRQ_FALLING, handler = toggle_mode)
+#loop
+while True:
+    while mode == 1:
+        light_on(light)
+    while mode == 2:
+        light_fade(light)
